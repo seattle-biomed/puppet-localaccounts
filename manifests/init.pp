764a771d2ef585f5cc0e665afd404c89a7e9a21e
Andy Leonard <andy.leonard@sbri.org>
2012-11-02 15:42:09 -0700

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,46 @@
+# == Class: localaccounts
+#
+# Create local users and groups.
+#
+# === Parameters
+#
+# [*groups*]
+#   Hiera hash of the form: <groupname>: { gid: <gidnumber> }
+#
+# [*users*]
+#   Hiera hash of the form: <username>: { uid: <uidnumber>, gid: gidnumber,
+#                                         comment: <gecos> }
+#
+# [*sshkeys*]
+#   Hiera hash of the form: <name>: { key: <key>, type: <type>, user: <user> }
+#
+# === Examples
+#
+#  include localaccounts
+#
+# === Requires
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard, Seattle Biomedical Research Institute
+#
+class localaccounts($groups, $users, $sshkeys) {
+
+  $defaults = { ensure => present }
+  
+  $user_defaults = {
+    ensure => present,
+    managehome => true,
+    shell => '/bin/bash' }
+  
+  create_resources(group, $groups, $defaults)
+
+  create_resources(user, $users, $user_defaults)
+
+  create_resources(ssh_authorized_key, $sshkeys, $defaults)
+  
+}
